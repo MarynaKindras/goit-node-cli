3550a5d7a723931017267a8fdb1a9fd621477ac5
MarynaKindras <maryna.kindras@gmail.com>
2024-05-01 16:10:16 +0300

    Done second task in contacts.js file
    In the contacts.js file, import the fs (in the version that works with promises - fs/promises) and path modules for working with the file system.
    Create a contactsPath variable and write the path to the contacts.json file in it. To compile the path, use the methods of the path module.
    Add asynchronous functions to work with the contact collection. In functions, use the fs module and its readFile() and writeFile() methods. The corresponding functions must return the necessary data using the return statement. Output to the console in written functions should not be carried out.
    Export the created functions.

---
+++ contacts.js
@@ -0,0 +1,47 @@
+const fs = require("fs").promises;
+const path = require("path");
+const { nanoid } = require("nanoid");
+
+const contactsPath = path.join(__dirname, "db/contacts.json");
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+}
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const contactById = contacts.find((contact) => contact.id === contactId);
+  return contactById || null;
+}
+
+async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+}
+
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
